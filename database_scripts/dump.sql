e4ff36d8d4c73c316feae6f43404f46589a2ebb1
Lucas Cruz <lucas.apcruz@gmail.com>
2023-01-26 12:28:25 -0300

    feat: new entities for body parts affected by diseases

--- database_scripts/dump.sql
+++ database_scripts/dump.sql
@@ -21,5 +21,22 @@ CREATE TABLE diseases (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+
+CREATE TABLE body_parts (
+	id SERIAL PRIMARY KEY,
+	part VARCHAR(150) NOT NULL,
+	type VARCHAR(150),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE body_parts_affected_by_diseases (
+	id SERIAL PRIMARY KEY,
+	body_part_id INTEGER NOT NULL,
+	disease_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
 ALTER TABLE aliases ADD CONSTRAINT aliases_fk0 FOREIGN KEY (gene_id) REFERENCES genes(id);
 ALTER TABLE diseases ADD CONSTRAINT diseases_fk0 FOREIGN KEY (gene_id) REFERENCES genes(id);
+ALTER TABLE body_parts_affected_by_diseases ADD CONSTRAINT bp_fk0 FOREIGN KEY (body_part_id) REFERENCES body_parts(id);
+ALTER TABLE body_parts_affected_by_diseases ADD CONSTRAINT bp_fk1 FOREIGN KEY (disease_id) REFERENCES diseases(id);
